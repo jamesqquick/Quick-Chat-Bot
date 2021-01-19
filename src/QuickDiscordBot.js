330dc044c1e0a9e8480669e65c2a6f9e07dba80b
James Quick <james.q.quick@outlook.com>
2021-01-18 18:36:05 -0600

    Updated logging for test mode. Also, not allowing testmode without a test channel name defined

--- src/QuickDiscordBot.js
+++ src/QuickDiscordBot.js
@@ -6,12 +6,18 @@ module.exports = class QuickDiscordBot {
         const { commandsDir, testChannel, testMode, botToken } = config;
         if (botToken === undefined || typeof botToken !== 'string') {
             throw new Error(
-                'Invalid configuration for TwitchChatBot. botToken is required.'
+                'Invalid configuration for DiscordChatBot. botToken is required.'
             );
         }
         if (commandsDir === undefined || typeof commandsDir !== 'string') {
             throw new Error(
-                'Invalid configuration for TwitchChatBot. Commands directory is required.'
+                'Invalid configuration for DiscordChatBot. Commands directory is required.'
+            );
+        }
+
+        if (testMode && (!testChannel || typeof testChannel !== 'string')) {
+            throw new Error(
+                'Invalid configuration for DiscordChatBot. testChannel is required to run in test mode.'
             );
         }
         this.config = config;
@@ -26,6 +32,11 @@ module.exports = class QuickDiscordBot {
     connect() {
         this.client.on('ready', () => {
             console.log('Learn Build Teach is active');
+            if (this.config.testMode && this.config.testChannel) {
+                console.log(
+                    `***RUNNING IN TEST MODE*** and only listening to channel ${this.config.testChannel}`
+                );
+            }
         });
         this.client.on('message', this.messageHandler.handleMessage);
         this.client.login(this.config.botToken);
